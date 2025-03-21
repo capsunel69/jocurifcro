a76740e4fc8364570374b59130c6e549785edf87
Alex (Căpșunel) <capsunel@stakeborg.com>
2025-03-21 18:33:53 +0200

    testing vultr server

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://192.168.0.54:5173',
   'https://fotbal-comedie.ro',
-  'http://140.82.39.237:3001'
+  'https://joblessai.ro'
 ];
 
 const app = express();
