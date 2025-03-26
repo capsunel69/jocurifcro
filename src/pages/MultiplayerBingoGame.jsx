d8bb6706c8c04362f360a69cc23a79cfbcea5e11
Alex (Căpșunel) <capsunel@stakeborg.com>
2025-03-26 16:09:23 +0200

    ready fix

--- src/pages/MultiplayerBingoGame.jsx
+++ src/pages/MultiplayerBingoGame.jsx
@@ -1456,7 +1456,7 @@ function MultiplayerBingoGame() {
                   onClick={handleReadyToggle}
                   w="full"
                 >
-                  {isReady ? "I'm not Ready!" : "Click when Ready"}
+                  {isReady ? "Ready!" : "Ready!"}
                 </Button>
                 
                 {/* Add Exit Room button here */}
